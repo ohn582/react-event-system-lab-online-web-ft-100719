2741709c120082e2542fe37fad269168aed0f1c6
Neal JIn Oh <no4259@gmail.com>
2020-02-23 05:35:00 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,11 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+class EyesOnMe extends Component{
+  render(){
+    return(
+      
+    )
+  }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
-// Code Keypad Component Here
+import React, { Component } from 'react';
+
+class Keypad extends Component{
+  render(){
+    return(
+      
+    )
+  }
+}
+
+export default Keypad;
